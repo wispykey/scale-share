d68b5495cb279c2cb693b7ab0528c934d9a0e9dc
Keelan <hui.keelan@gmail.com>
2026-01-28 07:40:41 -0800

    Improve test formatting

--- client/src/tests/expand-scale.test.ts
+++ client/src/tests/expand-scale.test.ts
@@ -12,10 +12,11 @@ test("should create C major one octave", () => {
     expect(expandScale(scale, {
         initialRegister: 4,
         minNote: note("C", 4),
-        maxNote: note("B", 7),
+        maxNote: note("B", 7), // set 'maxNote' deliberately high so that 'octaves' restricts range
         octaves: 1
     })).toEqual(
-        notes("C4 D4 E4 F4 G4 A4 B4 C5 B4 A4 G4 F4 E4 D4 C4")
+        notes("C4 D4 E4 F4 G4 A4 B4 C5 \
+            B4 A4 G4 F4 E4 D4 C4")
     );
 });
 
@@ -26,7 +27,10 @@ test("should create C major two octaves", () => {
         maxNote: note("B", 7),
         octaves: 2
     })).toEqual(
-        notes("C4 D4 E4 F4 G4 A4 B4 C5 D5 E5 F5 G5 A5 B5 C6 B5 A5 G5 F5 E5 D5 C5 B4 A4 G4 F4 E4 D4 C4")
+        notes("C4 D4 E4 F4 G4 A4 B4 C5 \
+            D5 E5 F5 G5 A5 B5 C6 \
+            B5 A5 G5 F5 E5 D5 C5 \
+            B4 A4 G4 F4 E4 D4 C4")
     );
 });
 
@@ -34,7 +38,7 @@ test("should create C major first five notes", () => {
     expect(expandScale(scale, {
         initialRegister: 4,
         minNote: note("C", 4),
-        maxNote: note("G", 4),
+        maxNote: note("G", 4), // set 'maxNote' deliberately low so that it restricts range instead of 'octaves'
         octaves: 1
     })).toEqual(
         notes("C4 D4 E4 F4 G4 F4 E4 D4 C4")
@@ -46,7 +50,10 @@ test("should create C major first five notes, then dip below to B3 and up", () =
         initialRegister: 4,
         minNote: note("B", 3),
         maxNote: note("G", 4),
+        // no octaves specified; should follow ascend-to-max, descend-to-min, ascend-to-start-position
     })).toEqual(
-        notes("C4 D4 E4 F4 G4 F4 E4 D4 C4 B3 C4")
+        notes("C4 D4 E4 F4 G4 \
+            F4 E4 D4 C4 \
+            B3 C4")
     );
 });
